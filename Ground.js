3f6e2dbd007b8dcd7d3702982f8a2f0b9dbafcd2
Marcel <marcel.mikolas@gmail.com>
2021-03-18 15:17:20 +0100

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -3,16 +3,10 @@ class Ground {
     static width = okno.lp;
     static x = 100;
     static y = okno.ln + 100;
-
     Render() {
         ctx.beginPath();
         ctx.rect(Ground.x, Ground.y, Ground.width, Ground.height);
         ctx.drawImage(GroundSkin, 0, Ground.y - Ground.height, Ground.width, Ground.height);
     }
-
-
-
-
-
 
 }
