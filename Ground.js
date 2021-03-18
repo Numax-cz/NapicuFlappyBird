3c64b00cda423cb83a8fae2b010555375ce485cc
Marcel Mikolas <marcel.mikolas@gmail.com>
2021-03-18 14:30:33 +0100

    Nastavení podlahy

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    static height = 250;
+    static width = okno.lp;
+    static x = 100;
+    static y = okno.ln + 100;
+
+    Render() {
+        ctx.beginPath();
+        ctx.rect(Ground.x, Ground.y, Ground.width, Ground.height);
+        ctx.drawImage(GroundSkin, 0, Ground.y - Ground.height, Ground.width, Ground.height);
+    }
+
+
+
+
+
+
+}
