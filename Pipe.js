afea6faf2c1fa8aa88e310a62cccbf4e101ed094
Marcel <marcel.mikolas@gmail.com>
2021-03-18 15:54:39 +0100

    Vytvořena náhodná generace pipe
    Pipe.js není hotov!!!!

--- Pipe.js
+++ Pipe.js
@@ -1,14 +1,29 @@
 class Pipe {
-    static height = 400;
+    static height = 800;
     static width = 200;
     static x = 800;
-    static y = 1200;
-
-
+    static y = 1600;
+    static RandomY = Math.floor(Math.random() * (1300 - 1000) + 1000)
+    static RandomOther = Math.floor(Math.random() * (1600 - 1300) + 1300);
+    //800 -rn
+    //1000 - min
+    //1300 - max
+    ////////////
+    //1500 - rn
+    //1300 - min
+    //1600 - max
 
     Render() {
         ctx.beginPath();
         ctx.rect(Pipe.x, Pipe.y, Pipe.width, Pipe.height);
-        ctx.drawImage(PipeSkin, Pipe.x, Pipe.y - Pipe.height, Pipe.width, Pipe.height);
+        ctx.drawImage(PipeSkinBottom, Pipe.x, Pipe.y - Pipe.height, Pipe.width, Pipe.height);
+        ctx.beginPath();
+        ctx.rect(Pipe.x, Pipe.y - Pipe.RandomY, Pipe.width, Pipe.height);
+        ctx.drawImage(PipeSkinTop, Pipe.x, (Pipe.y - Pipe.RandomY) - Pipe.height , Pipe.width, Pipe.height);
+        
+        
+        
     }
+    
+
 }
