7eab2a0178f920a3b3f8f0bab03f93089ca26857
Marcel <marcel.mikolas@gmail.com>
2021-03-18 17:39:57 +0100

    Základní tvorba trubek

--- Pipe.js
+++ Pipe.js
@@ -1,10 +1,16 @@
 class Pipe {
+
+    constructor(x) {
+        this.x = x;
+        this.y = Pipe.RandomPipeBottom();
+        this.y2 = Pipe.RandomPipeTop();
+    }
+
+
     static height = 800;
     static width = 200;
-    static x = 800;
-    static y = 1600;
-    static RandomY = Math.floor(Math.random() * (1300 - 1000) + 1000)
-    static RandomOther = Math.floor(Math.random() * (1600 - 1300) + 1300);
+    //static x = 600;
+
     //800 -rn
     //1000 - min
     //1300 - max
@@ -12,18 +18,39 @@ class Pipe {
     //1500 - rn
     //1300 - min
     //1600 - max
-
-    Render() {
-        ctx.beginPath();
-        ctx.rect(Pipe.x, Pipe.y, Pipe.width, Pipe.height);
-        ctx.drawImage(PipeSkinBottom, Pipe.x, Pipe.y - Pipe.height, Pipe.width, Pipe.height);
-        ctx.beginPath();
-        ctx.rect(Pipe.x, Pipe.y - Pipe.RandomY, Pipe.width, Pipe.height);
-        ctx.drawImage(PipeSkinTop, Pipe.x, (Pipe.y - Pipe.RandomY) - Pipe.height , Pipe.width, Pipe.height);
-        
-        
-        
+    static RandomPipeBottom() {
+        return Math.floor(Math.random() * (1600 - 1300) + 1300);
     }
-    
+    static RandomPipeTop() {
+        return Math.floor(Math.random() * (1300 - 1000) + 1000);
+    }
+    Render() {
+
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, Pipe.width, Pipe.height);
+        ctx.drawImage(PipeSkinBottom, this.x, this.y - Pipe.height, Pipe.width, Pipe.height);
+        ctx.beginPath();
+        ctx.rect(this.x, this.y - Pipe.RandomPipeTop(), Pipe.width, Pipe.height);
+        ctx.drawImage(PipeSkinTop, this.x, (this.y - this.y2) - Pipe.height, Pipe.width, Pipe.height);
+
+
+        if (Game.Status == true) {
+            this.Move();
+        }
+        
+        if ((this.x + Pipe.width + 20) < 0) {
+            this.x = okno.ln;
+            this.Reset();
+        }
+    }
+    Move() {
+        this.x -= 10;
+        Hrac.x += 0.05
+    }
+    Reset() {
+        this.y = Pipe.RandomPipeBottom();
+        this.y2 = Pipe.RandomPipeTop();
+    }
+
 
 }
