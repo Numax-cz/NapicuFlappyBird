abc5581061c03e26216ea6e61f45cca19455ef70
Marcel <marcel.mikolas@gmail.com>
2021-03-19 20:36:38 +0100

    Přidána funkce Collision()
    Funkce na detekci kolizí

--- Pipe.js
+++ Pipe.js
@@ -4,11 +4,14 @@ class Pipe {
         this.x = x;
         this.y = Pipe.RandomPipeBottom();
         this.y2 = Pipe.RandomPipeTop();
+
+        this.DefaultX = this.x;
     }
 
 
     static height = 800;
     static width = 200;
+    static Speed = 10
     //static x = 600;
 
     //800 -rn
@@ -32,20 +35,33 @@ class Pipe {
         ctx.beginPath();
         ctx.rect(this.x, this.y - Pipe.RandomPipeTop(), Pipe.width, Pipe.height);
         ctx.drawImage(PipeSkinTop, this.x, (this.y - this.y2) - Pipe.height, Pipe.width, Pipe.height);
-
-
         if (Game.Status == true) {
             this.Move();
         }
-        
         if ((this.x + Pipe.width + 20) < 0) {
             this.x = okno.ln;
             this.Reset();
+            Pipe.Speed += 0.05
+            Game.StatusPoint += 1
         }
+
     }
+
     Move() {
-        this.x -= 10;
-        Hrac.x += 0.05
+        this.x -= Pipe.Speed;
+    }
+    Collision(Player) {
+
+        if (Player.x > (this.x - Player.width) && Player.x < (this.x + Pipe.width)) {
+
+            if (Player.y < (this.y + Pipe.height) && Player.y > (this.y - Pipe.height)) {
+
+                Hrac.x = this.x - Player.width
+
+            }
+
+
+        }
     }
     Reset() {
         this.y = Pipe.RandomPipeBottom();
